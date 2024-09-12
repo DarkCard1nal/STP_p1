28dcad2e0a13b2cb67dc132457ec9e1b3a1b69ef
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-09-12 19:07:42 +0300

    Fixed value parsing, wrote game logic. 	modified:   p1.rb

--- p1.rb
+++ p1.rb
@@ -1,8 +1,10 @@
 puts('The game Rock-paper-scissors by Shkilnyi V. CS31')
 
 options = ['Rock', 'Paper', 'Scissors']
+messageWin = 'You Win!'
+messageLose = 'You Lose!'
 
-if ARGV.empty
+if ARGV.empty?
   puts('Make a choice:')
   puts("1 - #{options[0]}")
   puts("2 - #{options[1]}")
@@ -11,7 +13,42 @@ if ARGV.empty
 
   input = gets.chomp
 else
-  input = ARGV[0]
+  input = ARGV[0].dup
 end
 
-puts(input)
+input.strip!
+
+if input.casecmp(options[0]) == 0
+  number = 1
+elsif input.casecmp(options[1]) == 0
+  number = 2
+elsif input.casecmp(options[2]) == 0
+  number = 3
+elsif input.to_i != 0
+  number = input.to_i
+else
+  puts 'Input error, check if the input is correct'
+  return
+end
+
+number -= 1
+rundNumber = rand(0..2)
+
+puts("You: #{options[number]}")
+puts("Bot: #{options[rundNumber]}")
+
+if number == rundNumber
+  puts("Draw!")
+elsif number > rundNumber
+  if number == 2
+    puts(messageLose)
+  else
+    puts(messageWin)
+  end
+else
+  if rundNumber == 2
+    puts(messageWin)
+  else
+    puts(messageLose)
+  end
+end
