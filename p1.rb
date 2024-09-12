faafc11f601e018e05ef9ff3de8c2c57ac2e06c5
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-09-12 18:31:24 +0300

    Created handling of command line arguments and their absence 	modified:   p1.rb

--- p1.rb
+++ p1.rb
@@ -1,1 +1,17 @@
-puts("Hello Wolrld!")
+puts('The game Rock-paper-scissors by Shkilnyi V. CS31')
+
+options = ['Rock', 'Paper', 'Scissors']
+
+if ARGV.empty
+  puts('Make a choice:')
+  puts("1 - #{options[0]}")
+  puts("2 - #{options[1]}")
+  puts("3 - #{options[2]}")
+  puts("Enter your choice:")
+
+  input = gets.chomp
+else
+  input = ARGV[0]
+end
+
+puts(input)
