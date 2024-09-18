2c1ea6e3a74f2fa2efab1c3e9d1a215b29a69f92
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-09-18 23:32:16 +0300

    added environment settings, formatted code using rubocop with its settings 	new file:   .rubocop.yml 	new file:   .ruby-version 	new file:   2 	new file:   Gemfile 	new file:   Gemfile.lock 	modified:   p1.rb

--- p1.rb
+++ p1.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 puts('The game Rock-paper-scissors by Shkilnyi V. CS31')
 
-options = ['Rock', 'Paper', 'Scissors']
+options = %w[Rock Paper Scissors]
 messageWin = 'You Win!'
 messageLose = 'You Lose!'
 
 if ARGV.empty?
-  puts('Make a choice:')
-  puts("1 - #{options[0]}")
-  puts("2 - #{options[1]}")
-  puts("3 - #{options[2]}")
-  puts("Enter your choice:")
+	puts('Make a choice:')
+	puts("1 - #{options[0]}")
+	puts("2 - #{options[1]}")
+	puts("3 - #{options[2]}")
+	puts('Enter your choice:')
 
-  input = gets.chomp
+	input = gets.chomp
 else
-  input = ARGV[0].dup
+	input = ARGV[0].dup
 end
 
 input.strip!
 
-if input.casecmp(options[0]) == 0
-  number = 1
-elsif input.casecmp(options[1]) == 0
-  number = 2
-elsif input.casecmp(options[2]) == 0
-  number = 3
-elsif input.to_i != 0
-  number = input.to_i
+if input.casecmp(options[0]).zero?
+	number = 1
+elsif input.casecmp(options[1]).zero?
+	number = 2
+elsif input.casecmp(options[2]).zero?
+	number = 3
+elsif !input.to_i.zero?
+	number = input.to_i
 else
-  puts 'Input error, check if the input is correct'
-  return
+	puts 'Input error, check if the input is correct'
+	return
 end
 
 number -= 1
@@ -38,17 +40,15 @@ puts("You: #{options[number]}")
 puts("Bot: #{options[rundNumber]}")
 
 if number == rundNumber
-  puts("Draw!")
+	puts('Draw!')
 elsif number > rundNumber
-  if number == 2
-    puts(messageLose)
-  else
-    puts(messageWin)
-  end
+	if number == 2
+		puts(messageLose)
+	else
+		puts(messageWin)
+	end
+elsif rundNumber == 2
+	puts(messageWin)
 else
-  if rundNumber == 2
-    puts(messageWin)
-  else
-    puts(messageLose)
-  end
+	puts(messageLose)
 end
